b889436a887b845a4edbe18cb8a6cf1dca06f3e3
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-31 01:15:12 +0200

    Add style sheet for component

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const MeetingTypeList = () => {
     return (
-        <div>
-            MeetingTypeList
-        </div>
+        <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+            <div
+                className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px]">
+                box
+            </div>
+        </section>
     );
 };
 
