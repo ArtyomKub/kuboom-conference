eafc9988ba65be82683a0ae464bc8e4bbe0a5a6b
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-31 01:28:00 +0200

    Connect four component HomeCard in MeetingTypeList

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,12 +1,15 @@
+'use client'
 import React from 'react';
+import Image from 'next/image'
+import HomeCard from '@/components/HomeCard';
 
 const MeetingTypeList = () => {
     return (
         <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-            <div
-                className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px]">
-                box
-            </div>
+            <HomeCard/>
+            <HomeCard/>
+            <HomeCard/>
+            <HomeCard/>
         </section>
     );
 };
