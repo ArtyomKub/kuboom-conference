ebf0616ee26b91d57cda66e8e32676bbb367f841
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-30 23:29:15 +0200

    Add clerkMiddleware

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -57,8 +57,6 @@ const MobileNav = () => {
                                 })}
                             </section>
                         </SheetClose>
-
-
                     </div>
                 </SheetContent>
             </Sheet>
