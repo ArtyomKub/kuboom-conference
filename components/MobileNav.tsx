5b8af3ee116a903942809b56f1c2057b5f03aaca
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-30 19:37:30 +0200

    Add className with style for section

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const MobileNav = () => {
     return (
-        <div>
+        <section className="w-full max-w-[264px]">
             MobileNav
-        </div>
+        </section>
     );
 };
 
