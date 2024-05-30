8b7dbfc463601f038aafa9d3c6549f06be54ee31
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-30 11:06:21 +0200

    Rename function - Home

--- app/root/(home)/page.tsx
+++ app/root/(home)/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Page = () => {
+const Home = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
 
-export default Page;
+export default Home;
