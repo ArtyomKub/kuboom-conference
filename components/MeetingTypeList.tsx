b2fec806c5417f47c085f498d139722793f2d169
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-31 01:35:14 +0200

    Add props in first HomeCard

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,11 +1,18 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 import HomeCard from '@/components/HomeCard';
 
 const MeetingTypeList = () => {
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+
     return (
         <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-            <HomeCard/>
+            <HomeCard
+                img="/icons/add-meeting.svg"
+                title="New Meeting"
+                descriptions="Start an instant meeting"
+                handleClick={() => setMeetingState('isJoiningMeeting')}
+            />
             <HomeCard/>
             <HomeCard/>
             <HomeCard/>
