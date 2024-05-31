83f7c706b46370a3407c63be4c497bb1f2268432
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-31 02:51:16 +0200

    Add a button

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -8,6 +8,8 @@ import {
     DialogTrigger,
 } from '@/components/ui/dialog'
 import Image from 'next/image';
+import {cn} from '@/lib/utils';
+import {Button} from '@/components/ui/button';
 
 
 interface MeetingModalProps {
@@ -38,10 +40,19 @@ const MeetingModal = ({
                 className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
                 <div className="flex flex-col gap-6">
                     {image && (
-                        <div>
+                        <div className="flex justify-center">
                             <Image src={image} alt={'image'} width={72} height={72}/>
                         </div>
                     )}
+                    <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>{title}</h1>
+                    {children}
+                    <Button className="bg-blue-1 focus-visible:ring-0 focus-visible: ring-offset-0"
+                            onClick={handleClick}>
+                        {buttonIcon && (
+                            <Image src={buttonIcon} alt="button icon" width={13} height={13}/>
+                        )}
+                        {buttonText || 'Schedule Meeting'}
+                    </Button>
                 </div>
             </DialogContent>
         </Dialog>
