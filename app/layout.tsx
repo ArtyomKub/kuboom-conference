b857d852b9a2153cc7c2a3a74f324e517e940df4
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-01 13:26:05 +0200

    Import styles.css from stream-io

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google';
 import './globals.css';
 import {ClerkProvider} from '@clerk/nextjs';
 import React from 'react';
+import '@stream-io/video-react-sdk/dist/css/styles.css'
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({subsets: ['latin']});
