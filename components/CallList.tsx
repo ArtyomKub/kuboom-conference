10bb418198c2ed4679f7ae00518648d7cc10a2b9
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-02 22:18:24 +0200

    Connect MeetingCard with props type

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -44,7 +44,39 @@ const CallList = ({type}: { type: 'ended' | 'upcoming' | 'recordings' }) => {
     return (
         <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
             {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
-                <MeetingCard/>
+                <MeetingCard
+                    key={(meeting as Call).id}
+                    icon={
+                        type === 'ended'
+                            ? '/icons/previous.svg'
+                            : type === 'upcoming'
+                                ? '/icons/upcoming.svg'
+                                : '/icons/recordings.svg'
+                    }
+                    title={
+                        (meeting as Call).state?.custom?.description ||
+                        (meeting as CallRecording).filename?.substring(0, 20) ||
+                        'No Description'
+                    }
+                    date={
+                        (meeting as Call).state?.startsAt?.toLocaleString() ||
+                        (meeting as CallRecording).start_time?.toLocaleString()
+                    }
+                    isPreviousMeeting={type === 'ended'}
+                    link={
+                        type === 'recordings'
+                            ? (meeting as CallRecording).url
+                            : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`
+                    }
+                    buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
+                    buttonText={type === 'recordings' ? 'Play' : 'Start'}
+                    handleClick={
+                        type === 'recordings'
+                            ? () => router.push(`${(meeting as CallRecording).url}`)
+                            : () => router.push(`/meeting/${(meeting as Call).id}`)
+                    }
+
+                />
             )) : (
                 <h1>{noCallMessage}</h1>
             )}
