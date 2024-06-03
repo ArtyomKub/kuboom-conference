dc318e03b83fadbc1d5e61d4a7a9cb318a89e7ef
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-03 15:17:11 +0200

    Fix bugs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,68 +1,124 @@
-import type {Config} from 'tailwindcss';
+// import type { Config } from "tailwindcss"
+//
+// const config = {
+//   darkMode: ["class"],
+//   content: [
+//     './pages/**/*.{ts,tsx}',
+//     './components/**/*.{ts,tsx}',
+//     './app/**/*.{ts,tsx}',
+//     './src/**/*.{ts,tsx}',
+// 	],
+//   prefix: "",
+//   theme: {
+//     container: {
+//       center: true,
+//       padding: "2rem",
+//       screens: {
+//         "2xl": "1400px",
+//       },
+//     },
+//     extend: {
+//       colors: {
+//         dark: {
+//           1: '#1C1F2E',
+//           2: '#161925'
+//         },
+//         blue: {
+//           1: '#0e78f9'
+//         },
+//         sky: {
+//           1: '#C9DDFF'
+//         },
+//       },
+//       backgroundImage: {
+//         hero: "url('/images/hero-background.png')"
+//       },
+//       keyframes: {
+//         "accordion-down": {
+//           from: { height: "0" },
+//           to: { height: "var(--radix-accordion-content-height)" },
+//         },
+//         "accordion-up": {
+//           from: { height: "var(--radix-accordion-content-height)" },
+//           to: { height: "0" },
+//         },
+//       },
+//       animation: {
+//         "accordion-down": "accordion-down 0.2s ease-out",
+//         "accordion-up": "accordion-up 0.2s ease-out",
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// } satisfies Config
+//
+// export default config
+
+import type { Config } from 'tailwindcss';
 
 const config = {
-    darkMode: ['class'],
-    content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}',
-    ],
-    prefix: '',
-    theme: {
-        container: {
-            center: true,
-            padding: '2rem',
-            screens: {
-                '2xl': '1400px',
-            },
-        },
-        extend: {
-            colors: {
-                dark: {
-                    1: '#1C1F2E',
-                    2: '#161925',
-                    3: '#252A41',
-                    4: '#1E2757',
-                },
-                blue: {
-                    1: '#0E78F9',
-                },
-                sky: {
-                    1: '#C9DDFF',
-                    2: '#ECF0FF',
-                    3: '#F5FCFF',
-                },
-                orange: {
-                    1: '#FF742E',
-                },
-                purple: {
-                    1: '#830EF9',
-                },
-                yellow: {
-                    1: '#F9A90E',
-                },
-            },
-            keyframes: {
-                'accordion-down': {
-                    from: {height: '0'},
-                    to: {height: 'var(--radix-accordion-content-height)'},
-                },
-                'accordion-up': {
-                    from: {height: 'var(--radix-accordion-content-height)'},
-                    to: {height: '0'},
-                },
-            },
-            animation: {
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
-            },
-            backgroundImage: {
-                hero: 'url(\'/images/hero-background.png\')',
-            },
-        },
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
     },
-    plugins: [require('tailwindcss-animate')],
+    extend: {
+      colors: {
+        dark: {
+          1: '#656ea8',
+          2: '#414470',
+          3: '#252A41',
+          4: '#1E2757',
+        },
+        blue: {
+          1: '#5e9df3',
+        },
+        sky: {
+          1: '#C9DDFF',
+          2: '#ECF0FF',
+          3: '#F5FCFF',
+        },
+        orange: {
+          1: '#e18e5f',
+        },
+        purple: {
+          1: '#d2058a',
+        },
+        yellow: {
+          1: 'rgba(241,201,115,0.85)',
+        },
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      backgroundImage: {
+        hero: "url('/images/hero-background.png')",
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
