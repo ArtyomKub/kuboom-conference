0ea6eb74c979e0cb678de0d4d847322cbbd7b332
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-03 13:40:22 +0200

    Refactoring Code = Gets a beautiful CODE

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,6 @@
-// import React from 'react';
-// import Link from 'next/link';
-// import Image from 'next/image';
-// import MobileNav from '@/components/MobileNav';
-// import {SignedIn, UserButton} from '@clerk/nextjs';
-//
-//
-// const Navbar = () => {
-//     return (
-//         <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-//             <Link href="/" className="flex items-center gap-1">
-//                 <Image
-//                     src="/icons/logo.svg"
-//                     width={32}
-//                     height={32}
-//                     alt="Yoom logo"
-//                     className="max-sm:size-10"
-//                 />
-//                 <p className="text-[26px] font-extrabold text-white max-sm:hidden">Kuboom</p>
-//             </Link>
-//             <div className="flex-between gap-5">
-//                 <SignedIn>
-//                     <UserButton/>
-//                 </SignedIn>
-//
-//                 <MobileNav/>
-//             </div>
-//         </nav>
-//     );
-// };
-//
-// export default Navbar;
 import Image from 'next/image';
 import Link from 'next/link';
-import { SignedIn, UserButton } from '@clerk/nextjs';
+import {SignedIn, UserButton} from '@clerk/nextjs';
 
 import MobileNav from './MobileNav';
 
@@ -53,10 +21,10 @@ const Navbar = () => {
             </Link>
             <div className="flex-between gap-5">
                 <SignedIn>
-                    <UserButton afterSignOutUrl="/sign-in" />
+                    <UserButton afterSignOutUrl="/sign-in"/>
                 </SignedIn>
 
-                <MobileNav />
+                <MobileNav/>
             </div>
         </nav>
     );
